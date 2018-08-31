fef3e1350ef0d936923d974f7cdc2013a2b4a1de
kihong choi <khchoi0301@gmail.com>
2018-08-31 14:37:27 +0900

    Complete twittler_include add twittle func

--- data_generator.js
+++ data_generator.js
@@ -5,23 +5,31 @@
 
 // set up data structures
 window.streams = {};
+streams.input=[];
 streams.home = [];
 streams.users = {};
 streams.users.ingikim = [];
 streams.users.yannheo = [];
 streams.users.hoyoungjung = [];
 streams.users.julie = [];
+
 window.users = Object.keys(streams.users);
 
+//console.log(new Date());
+//console.log(streams)
+//console.log(streams.users)
+//console.log(window.users)
 // utility function for adding tweets to our data structures
-var addTweet = function(newTweet){
+
+var addTweet = function(newTweet){  //6
   var username = newTweet.user;
   streams.users[username].push(newTweet);
   streams.home.push(newTweet);
+  
 };
 
 // utility function
-var randomElement = function(array){
+var randomElement = function(array){  //3 //5
   var randomIndex = Math.floor(Math.random() * array.length);
   return array[randomIndex];
 };
@@ -33,33 +41,40 @@ var objects = ['my', 'your', 'the', 'a', 'my', 'an entire', 'this', 'that', 'the
 var nouns = ['cat', 'koolaid', 'system', 'city', 'worm', 'cloud', 'potato', 'money', 'way of life', 'belief system', 'security system', 'bad decision', 'future', 'life', 'pony', 'mind'];
 var tags = ['#techlife', '#burningman', '#sf', 'but only i know how', 'for real', '#sxsw', '#ballin', '#omg', '#yolo', '#magic', '', '', '', ''];
 
-var randomMessage = function(){
+var randomMessage = function(){ //4
   return [randomElement(opening), randomElement(verbs), randomElement(objects), randomElement(nouns), randomElement(tags)].join(' ');
 };
 
 // generate random tweets on a random schedule
-var generateRandomTweet = function(){
+var generateRandomTweet = function(){ //2
   var tweet = {};
-  tweet.user = randomElement(users);
+  //console.log(users);
+  tweet.user = randomElement(users);  // users가 뭔지?? window.users
   tweet.message = randomMessage();
-  tweet.created_at = new Date();
+  tweet.created_at = new Date();      // Date 없음 정의해야할듯.
   addTweet(tweet);
 };
 
-for(var i = 0; i < 10; i++){
+for(var i = 0; i < 10; i++){    // 1
   generateRandomTweet();
 }
 
-var scheduleNextTweet = function(){
+var scheduleNextTweet = function(){ 
+  
   generateRandomTweet();
-  setTimeout(scheduleNextTweet, Math.random() * 1500);
+  setTimeout(function(){
+//    streams.home=[]  
+   // console.log(streams.home)
+    scheduleNextTweet()}, Math.random() * 15000);
 };
 scheduleNextTweet();
 
 // utility function for letting students add "write a tweet" functionality
 // (note: not used by the rest of this file.)
-var writeTweet = function(message){
-  if(!visitor){
+var writeTweet = function(message,visitor){
+
+  console.log(users,!users.includes(visitor))
+  if(!users.includes(visitor)){
     throw new Error('set the global visitor property!');
   }
   var tweet = {};
